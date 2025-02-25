0d8b073917df870414b6d862988fa5204c33d600
joelepore <giuseppejoelepore@gmail.com>
2025-02-25 15:23:29 +0100

    feat: implement SearchModal with AccordionType and InputNumber components

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputNumber from './InputNumber'
+import { useGlobalContext } from '../context/GlobalContext';
+import Button from './Button';
+import AccordionType from './AccordionType';
 
 const SearchModal = () => {
+    const { filterData, setFilterData, resetModalFilterData, types } = useGlobalContext();
+
+    const handleSearchChange = (e) => {
+        setFilterData(prev => ({ ...prev, search: e.target.value }))
+    }
+    const handleRoomsChange = (value) => {
+        setFilterData(prev => ({ ...prev, rooms: value }));
+    }
+    const handleBedsChange = (value) => {
+        setFilterData(prev => ({ ...prev, beds: value }));
+    }
     return (
-        <div>SearchModal</div>
+        <div className='search-modal-overlay'>
+
+            <div className='search-modal p-3 gap-3 rounded'>
+                <input
+                    type="text"
+                    placeholder='Città o indirizzo'
+                    className='text-center py-2 searchbar'
+                    onChange={handleSearchChange}
+                    value={filterData.search}
+                />
+                <AccordionType types={types} />
+                <InputNumber
+                    label="Numero stanze"
+                    value={filterData.rooms}
+                    onChange={handleRoomsChange}
+                />
+                <InputNumber
+                    label="Numero letti"
+                    value={filterData.beds}
+                    onChange={handleBedsChange}
+                />
+
+                <div className='d-flex justify-content-between'>
+                    <button className="button-link" onClick={resetModalFilterData}>Cancella</button>
+                    <Button text="Cerca immobili" />
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/TypeButton.jsx
@@ -0,0 +1,7 @@
+const TypeButton = ({ text }) => {
+    return (
+        <button className="button button-type">{text}</button>
+    )
+}
+
+export default TypeButton
