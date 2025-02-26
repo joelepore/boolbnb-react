45bc17c997da2052001a97afbeb2d2b485687127
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-26 10:24:00 +0100

    feat: add like functionality to Card component and update LikesButton to handle likes

--- src/components/LikesButton.jsx
+++ src/components/LikesButton.jsx
@@ -1,6 +1,14 @@
-const LikesButton = () => {
+import { useGlobalContext } from "../context/GlobalContext";
+
+const LikesButton = ({ id }) => {
+
+    const { updateLikes } = useGlobalContext();
+
+
     return (
-        <button className="heart-button z-3">
+        <button
+            className="heart-button z-3"
+            onClick={() => updateLikes(id)}>
             <i className="fa-solid fa-heart"></i>
         </button>
     );
