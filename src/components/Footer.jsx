4fd2abe0a36e2827ab06bcbfa6c62f79b54b8fc8
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-26 10:39:30 +0100

    feat: update Footer component layout and styling; adjust link alignment and spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ const Footer = () => {
     return (
         <footer className="footer-custom footer bg-dark mt-5 py-3 text-center text-light">
             <div className="container">
-                <p className="mb-3 text-secondary">Domande? NON contattarci al:</p>
+                <p className="mb-3 text-secondary">Domande? <br /> NON contattarci al:</p>
                 <a href="" className="text-primary text-decoration-none">123-456-789</a>
 
-                <div className="footer-links d-flex justify-content-center flex-wrap pt-3">
+                <div className="footer-links d-flex justify-content-center flex-wrap pt-3 mt-2">
                     <a href="" className="text-secondary mx-3">Privacy</a>
                     <a href="" className="text-secondary mx-3">Termini di servizio</a>
                     <a href="" className="text-secondary mx-3">Centro assistenza</a>
                     <a href="" className="text-secondary mx-3">Account</a>
                 </div>
 
-                <div className="social-icons mt-3">
+                <div className="social-icons mt-4">
                     <i className="fab fa-facebook-f mx-2 text-primary"></i>
                     <i className="fab fa-twitter mx-2 text-primary"></i>
                     <i className="fab fa-instagram mx-2 text-primary"></i>
