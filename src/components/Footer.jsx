16aff1cafb82adeab994fa9b3f44a19d6de9fde5
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-26 10:29:48 +0100

    feat: enhance Footer component with contact info, links, and social icons; update scrollbar background color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+
 
 const Footer = () => {
     return (
-        <div>Footer</div>
-    )
-}
+        <footer className="footer-custom footer bg-dark mt-5 py-3 text-center text-light">
+            <div className="container">
+                <p className="mb-3 text-secondary">Domande? NON contattarci al:</p>
+                <a href="" className="text-primary text-decoration-none">123-456-789</a>
+
+                <div className="footer-links d-flex justify-content-center flex-wrap pt-3">
+                    <a href="" className="text-secondary mx-3">Privacy</a>
+                    <a href="" className="text-secondary mx-3">Termini di servizio</a>
+                    <a href="" className="text-secondary mx-3">Centro assistenza</a>
+                    <a href="" className="text-secondary mx-3">Account</a>
+                </div>
+
+                <div className="social-icons mt-3">
+                    <i className="fab fa-facebook-f mx-2 text-primary"></i>
+                    <i className="fab fa-twitter mx-2 text-primary"></i>
+                    <i className="fab fa-instagram mx-2 text-primary"></i>
+                </div>
+
+                <p className="mt-3 text-secondary"> ® 2024 BoolBnB</p>
+            </div>
+        </footer>
+    );
+};
+
 
 export default Footer
