5defea68ceda1bb3a930d590334c242c60bc8a0b
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-27 18:26:17 +0100

    fix: update Footer and NavBar components for improved navigation and search functionality; enhance CSS for better styling and interaction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,19 @@ const Footer = () => {
         <footer className="footer-custom footer mt-5 py-3 border-top text-center text-light">
             <div className="container">
                 <p className="mb-3 text-secondary">Domande? <br /> NON contattarci al:</p>
-                <a href="" className="text-primary text-decoration-none">123-456-789</a>
+                <a href="/" className="text-primary text-decoration-none">123-456-789</a>
 
                 <div className="footer-links d-flex justify-content-center flex-wrap pt-3 mt-2">
-                    <a href="" className="text-secondary mx-3">Privacy</a>
-                    <a href="" className="text-secondary mx-3">Termini di servizio</a>
-                    <a href="" className="text-secondary mx-3">Centro assistenza</a>
-                    <a href="" className="text-secondary mx-3">Account</a>
+                    <a href="/" className="text-secondary mx-3">Privacy</a>
+                    <a href="/" className="text-secondary mx-3">Termini di servizio</a>
+                    <a href="/" className="text-secondary mx-3">Centro assistenza</a>
+                    <a href="/" className="text-secondary mx-3">Account</a>
                 </div>
 
                 <div className="social-icons mt-4">
-                    <i className="fab fa-facebook-f mx-2 text-primary"></i>
-                    <i className="fab fa-twitter mx-2 text-primary"></i>
-                    <i className="fab fa-instagram mx-2 text-primary"></i>
+                    <a href="/"><i className="fab fa-facebook-f mx-2 text-primary"></i></a>
+                    <a href="/"><i className="fab fa-twitter mx-2 text-primary"></i></a>
+                    <a href="/"><i className="fab fa-instagram mx-2 text-primary"></i></a>
                 </div>
 
                 <p className="mt-3 text-secondary"> ® 2024 BoolBnB</p>
