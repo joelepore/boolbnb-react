220faa5eeab8434a9bf56dcf6bdc3d6a839ea409
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-28 11:19:18 +0100

    fix: improve button interaction in TypesMenu component with hover and active effects

--- src/components/TypesMenu.jsx
+++ src/components/TypesMenu.jsx
@@ -3,8 +3,8 @@
 const TypesMenu = ({ text, path, onClick }) => {
     return (
 
-        <button className="btn-type mx-4 mx-md-5">
-            <img src={`http://localhost:3000${path}`} alt={text} onClick={onClick} className="type-logo" />
+        <button className="btn-type mx-4 mx-md-5" onClick={onClick}>
+            <img src={`http://localhost:3000${path}`} alt={text} className="type-logo" />
             <h5>{text}</h5>
         </button>
 
