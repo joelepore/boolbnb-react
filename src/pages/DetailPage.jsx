d1d214b523d3a9877b20e8ea59b865a37ef9c2ba
Francesca-Lisi <francescalisi2143@gmail.com>
2025-02-28 16:58:54 +0100

    Fix: star component in Stars, detailPage and index.css

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,5 +1,4 @@
 import Stars from "../components/Stars"
-
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useGlobalContext } from "../context/GlobalContext"
@@ -12,15 +11,19 @@ const DetailPage = () => {
 
     useEffect(() => {
         fetchProperty(id)
-
     }, [])
 
     const navigate = useNavigate()
 
+    const totalVotes = property.reviews.reduce((sum, review) => sum + review.vote, 0);
+    const averageVote = (totalVotes / property.reviews.length).toFixed(2);
+
+
     return (
         <div>
             <h1 className='text-center'>Detail Page</h1>
-            <Stars vote={property.average_vote} />
+            {averageVote && <Stars vote={averageVote} />}
+            {/* <h1>{property.title}</h1> */}
             {/* Img di copertina con gallery affiancata CLICCANDO LE IMG APRE UN VISUALIZZATORE DI IMG */}
             {/* div dei dettagli che al suo interno ha le info dell'immobile e i COMPONENTI delle stelline e il cuore */}
             {/* componente slider rewiev che contiene il componente di rewievcard */}
