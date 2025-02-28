327db62bd822fc15bdc1d9548e2167424fa9af2a
Francesca-Lisi <francescalisi2143@gmail.com>
2025-02-28 20:49:04 +0100

    feat: add ReviewSlider and ReviewCard

--- src/components/ReviewSlider.jsx
+++ src/components/ReviewSlider.jsx
@@ -1,9 +1,60 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import ReviewCard from './ReviewCard'
 
-const ReviewSlider = () => {
+
+const ReviewSlider = ({ review }) => {
     return (
-        <div>ReviewSlider</div>
+
+        <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            pagination={{
+                clickable: true,
+            }}
+            navigation={{
+                nextEl: '.custom-next',
+                prevEl: '.custom-prev',
+            }}
+            breakpoints={{
+
+                320: {
+                    slidesPerView: 1, // Mostra una slide per volta
+                    spaceBetween: 10,  // Spazio tra le slide
+                },
+
+                768: {
+                    slidesPerView: 2, // Mostra tre slide per volta
+                    spaceBetween: 30,  // Spazio tra le slide
+                },
+
+                1024: {
+                    slidesPerView: 3, // Mostra quattro slide per volta
+                    spaceBetween: 30,  // Spazio tra le slide
+                },
+            }}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+        >
+            {review.map((rev) => (
+                <SwiperSlide key={rev.id}>
+                    <ReviewCard review={rev} />
+                </SwiperSlide>
+            ))}
+
+            <div className="swiper-button-prev custom-prev" ></div>
+            <div className="swiper-button-next custom-next" ></div>
+        </Swiper>
+
     )
 }
+
+
+
 
 export default ReviewSlider
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -5,6 +5,7 @@ import { useGlobalContext } from "../context/GlobalContext"
 import { useNavigate } from "react-router-dom"
 import LikesButton from "../components/LikesButton"
 import Button from "../components/Button"
+import ReviewSlider from "../components/ReviewSlider"
 
 const DetailPage = () => {
 
@@ -23,49 +24,55 @@ const DetailPage = () => {
 
 
     return (
-        <div className="container details-container bg-white p-3">
-            <div className="row align-items-center">
-                <div className="col-10">
-                    <h1 className="">{property.title}</h1>
-                </div>
+        <>
+            <div className="container details-container bg-white p-3">
+                <div className="row align-items-center">
+                    <div className="col-10">
+                        <h1 className="">{property.title}</h1>
+                    </div>
 
-                <div className="col-2 d-flex justify-content-end align-items-center">
-                    <h1 className="m-0 position-relative">
-                        <LikesButton id={id} callback={() => fetchProperty(id)} /> {property.likes}
-                    </h1>
-                </div>
-            </div>
-
-            <div className="details-container-img d-flex justify-content-center mt-4 py-3 bg-body-secondary">
-                <img src={property.cover_img} alt="property.img" className="details-img" />
-            </div>
-
-            <div className="row mt-5 d-flex">
-                <div className="col-9">
-                    <h2 className="text-truncate">{property.address}</h2>
-                    <p className="text-muted mt-2"><strong>Proprietario:</strong> {property.owner_fullname}</p>
-                    <p className="text-muted mt-1"><strong>email:</strong> {property.email}</p>
-
-                    <br />
-
-                    <div className="d-flex gap-4">
-                        <h3>Dettagli:</h3>
-                        <p>m²: {property.sqm}</p>
-                        <p><i className="fa-solid fa-door-open"></i> {property.rooms} </p>
-                        <p><i className="fa-solid fa-bed"></i> {property.beds} </p>
-                        <p><i className="fa-solid fa-bath"></i> {property.bathrooms} </p>
+                    <div className="col-2 d-flex justify-content-end align-items-center">
+                        <h1 className="m-0 position-relative">
+                            <LikesButton id={id} callback={() => fetchProperty(id)} /> {property.likes}
+                        </h1>
                     </div>
                 </div>
-                <div className="col-3 d-flex gap-3 flex-column">
-                    {averageVote && <Stars vote={averageVote} />}
-                    <p><i className="fa-solid fa-heart text-danger px-2"></i>{property.likes}</p>
-                    <Button className="" text={"Contatta proprietario"} />
+
+                <div className="details-container-img d-flex justify-content-center mt-4 py-3 bg-body-secondary">
+                    <img src={property.cover_img} alt="property.img" className="details-img" />
                 </div>
 
+                <div className="row mt-5 d-flex">
+                    <div className="col-9">
+                        <h2 className="text-truncate">{property.address}</h2>
+                        <p className="text-muted mt-2"><strong>Proprietario:</strong> {property.owner_fullname}</p>
+                        <p className="text-muted mt-1"><strong>email:</strong> {property.email}</p>
+
+                        <br />
+
+                        <div className="d-flex gap-4">
+                            <h3>Dettagli:</h3>
+                            <p>m²: {property.sqm}</p>
+                            <p><i className="fa-solid fa-door-open"></i> {property.rooms} </p>
+                            <p><i className="fa-solid fa-bed"></i> {property.beds} </p>
+                            <p><i className="fa-solid fa-bath"></i> {property.bathrooms} </p>
+                        </div>
+                    </div>
+                    <div className="col-3 d-flex gap-3 flex-column">
+                        {averageVote && <Stars vote={averageVote} />}
+                        <p><i className="fa-solid fa-heart text-danger px-2"></i>{property.likes}</p>
+                        <Button className="" text={"Contatta proprietario"} />
+                    </div>
+
+                </div>
+                <div className="col-12 mt-5">{property.description}</div>
             </div>
-            <div className="col-12 mt-5">{property.description}</div>
-            <div className="col-12 mt-5">INSERIRE RECENSIONE</div>
-        </div>
+
+            <div className="container bg-white my-3 p-3 pb-4">
+                <h1 className="mt-2 mb-3">Recensioni</h1>
+                <ReviewSlider review={property.reviews} />
+            </div>
+        </>
 
     )
 }
