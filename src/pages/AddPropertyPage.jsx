3722b2dfdd00240e13ab5cf2a78a6ed76411923f
joelepore <giuseppejoelepore@gmail.com>
2025-03-03 11:05:29 +0100

    feat: add client side validation in AddPropertyPage

--- src/pages/AddPropertyPage.jsx
+++ src/pages/AddPropertyPage.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import InputNumber from "../components/InputNumber";
 import axios from "axios";
 import { useGlobalContext } from "../context/GlobalContext";
+import validator from 'validator';
+import { useNavigate } from "react-router-dom";
 
 const AddPropertyPage = () => {
     const api_url = import.meta.env.VITE_API_URL
@@ -10,9 +12,9 @@ const AddPropertyPage = () => {
         title: '',
         address: '',
         description: '',
-        rooms: 1,
-        beds: 1,
-        bathrooms: 1,
+        rooms: 0,
+        beds: 0,
+        bathrooms: 0,
         sqm: 0,
         owner_fullname: '',
         email: '',
@@ -23,11 +25,91 @@ const AddPropertyPage = () => {
     const [step, setStep] = useState(1);
     const [propertyData, setPropertyData] = useState(initialPropertyData);
     const [previewImg, setPreviewImg] = useState('');
-    const [validationData, setValidationData] = useState({});
+    const [validationData, setValidationData] = useState({
+        title: true,
+        address: true,
+        description: true,
+        category: true
+    });
+    const navigate = useNavigate();
 
-    // const validateData = () => {
-    //     if(!title)
-    // }
+    // Funzione che valida i dati in input, setta a true il rispettivo stato in caso di errore 
+    // e restituisce true se c'e' almeno un errore 
+    const validateData = () => {
+        const { title, address, description, category, rooms, beds, bathrooms, sqm, cover_img, owner_fullname, email } = propertyData;
+        switch (step) {
+            case 1:
+                if (!title || title.length < 10) {
+                    setValidationData(prev => ({ ...prev, title: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, title: false }));
+                }
+                if (!address || address.length < 6) {
+                    setValidationData(prev => ({ ...prev, address: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, address: false }));
+                }
+
+                if (!description || description.length < 10) {
+                    setValidationData(prev => ({ ...prev, description: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, description: false }));
+                }
+
+                if (!category) {
+                    setValidationData(prev => ({ ...prev, category: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, category: false }));
+                }
+                break;
+            case 2:
+                if (rooms === 0) {
+                    setValidationData(prev => ({ ...prev, rooms: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, rooms: false }));
+                }
+                if (beds === 0) {
+                    setValidationData(prev => ({ ...prev, beds: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, beds: false }));
+                }
+                if (bathrooms === 0) {
+                    setValidationData(prev => ({ ...prev, bathrooms: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, bathrooms: false }));
+                }
+                if (sqm < 10) {
+                    setValidationData(prev => ({ ...prev, sqm: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, sqm: false }));
+                }
+                break;
+            case 3:
+                if (!cover_img) {
+                    setValidationData(prev => ({ ...prev, cover_img: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, cover_img: false }));
+                }
+                break;
+            case 4:
+                if (!owner_fullname || !owner_fullname.trim().includes(' ')) {
+                    setValidationData(prev => ({ ...prev, owner_fullname: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, owner_fullname: false }));
+                }
+                if (!email || !validator.isEmail(email)) {
+                    setValidationData(prev => ({ ...prev, email: true }));
+                } else {
+                    setValidationData(prev => ({ ...prev, email: false }));
+                }
+                break;
+        }
+
+    }
+
+    const isValidationOk = () => {
+        return Object.values(validationData).every(item => item === false);
+    }
 
     const handleInputTextChange = (e) => {
         const { name, value } = e.target;
@@ -39,9 +121,9 @@ const AddPropertyPage = () => {
     }
 
     const handleTypeChange = (e) => {
-        const {value} = e.target;
+        const { value } = e.target;
 
-        setPropertyData(prev => ({...prev, category: parseInt(value)}));
+        setPropertyData(prev => ({ ...prev, category: parseInt(value) }));
     }
 
     const handleRoomsChange = (value) => {
@@ -64,7 +146,6 @@ const AddPropertyPage = () => {
     const handleUpload = async () => {
         const formData = new FormData();
         for (let key in propertyData) {
-            console.log(key, propertyData[key]);
             formData.append(key, propertyData[key]);
         }
 
@@ -72,6 +153,7 @@ const AddPropertyPage = () => {
             const response = await axios.post(`${api_url}/properties`, formData, {
                 headers: { "Content-Type": "multipart/form-data" },
             });
+            navigate(`/property/${response.data.id}`);
         } catch (error) {
             console.error("Errore durante il caricamento", error);
         }
@@ -79,11 +161,19 @@ const AddPropertyPage = () => {
     }
 
     const handleNextStepClick = () => {
-        setStep(prev => prev + 1);
+        if (isValidationOk()) {
+            setStep(prev => prev + 1);
+        }
     }
     const handlePreviousStepClick = () => {
-        setStep(prev => prev - 1);
+        if (isValidationOk()) {
+            setStep(prev => prev - 1);
+        }
     }
+
+    useEffect(() => {
+        validateData();
+    }, [propertyData, step])
 
     return (
         <div className="container px-5 max-w-md">
@@ -102,7 +192,7 @@ const AddPropertyPage = () => {
                             onChange={handleInputTextChange}
                             name='title'
                         />
-                        <small className="text-danger">Il titolo deve avere almeno 10 caratteri.</small>
+                        {validationData.title && <small className="text-danger">Il titolo deve avere almeno 10 caratteri.</small>}
                     </div>
                     <div className="form-property">
                         <label htmlFor="input-property-address">Indirizzo</label>
@@ -115,7 +205,7 @@ const AddPropertyPage = () => {
                             onChange={handleInputTextChange}
                             name='address'
                         />
-                        <small className="text-danger">L'indirizzo deve avere almeno 6 caratteri.</small>
+                        {validationData.address && <small className="text-danger">L'indirizzo deve avere almeno 6 caratteri.</small>}
                     </div>
                     <div className="form-property">
                         <label htmlFor="input-property-description">Descrizione</label>
@@ -128,7 +218,7 @@ const AddPropertyPage = () => {
                             name='description'
                         >
                         </textarea>
-                        <small className="text-danger">La descrizione deve avere almeno 10 caratteri.</small>
+                        {validationData.description && <small className="text-danger">La descrizione deve avere almeno 10 caratteri.</small>}
                     </div>
                     <div className="form-property">
                         <label htmlFor="input-property-type">Categoria</label>
@@ -144,7 +234,7 @@ const AddPropertyPage = () => {
                                 <option key={type.id} value={type.id}>{type.name}</option>
                             ))}
                         </select>
-                        <small className="text-danger">La categoria è obbligatoria.</small>
+                        {validationData.category && <small className="text-danger">La categoria è obbligatoria.</small>}
 
                     </div>
                 </>
@@ -152,27 +242,30 @@ const AddPropertyPage = () => {
 
             {step === 2 && (
                 <>
-                    <div className="form-property">
+                    <div className="form-property text-center">
                         <InputNumber
                             label='Inserisci numero stanze'
                             onChange={handleRoomsChange}
                             value={propertyData.rooms}
                         />
+                        {validationData.rooms && <small className="text-danger">Il numero di stanze deve essere almeno 1</small>}
                     </div>
-                    <div className="form-property">
+                    <div className="form-property text-center">
                         <InputNumber
                             label='Inserisci numero letti'
                             onChange={handleBedsChange}
                             value={propertyData.beds}
 
                         />
+                        {validationData.beds && <small className="text-danger">Il numero di letti deve essere almeno 1</small>}
                     </div>
-                    <div className="form-property">
+                    <div className="form-property text-center">
                         <InputNumber
                             label='Inserisci numero bagni'
                             onChange={handleBathroomsChange}
                             value={propertyData.bathrooms}
                         />
+                        {validationData.bathrooms && <small className="text-danger">Il numero di bagni deve essere almeno 1</small>}
                     </div>
                     <div className="form-property text-center">
                         <label htmlFor="input-property-sqm">Inserisci i metri quadri</label>
@@ -185,6 +278,7 @@ const AddPropertyPage = () => {
                             onChange={handleInputTextChange}
                             name="sqm"
                         />
+                        {validationData.sqm && <small className="text-danger">Il numero di metri quadri non è valido </small>}
                     </div>
                 </>
             )}
@@ -201,6 +295,7 @@ const AddPropertyPage = () => {
                             accept="image/*"
                             onChange={handleImageChange}
                         />
+                        {validationData.cover_img && <small className="text-danger">L'immagine di copertina è obbligatoria</small>}
                     </div>
                     {previewImg && (
                         <picture className="">
@@ -223,6 +318,7 @@ const AddPropertyPage = () => {
                             onChange={handleInputTextChange}
                             name='owner_fullname'
                         />
+                        {validationData.owner_fullname && <small className="text-danger">Il nome e cognome del proprietario sono obbligatori</small>}
                     </div>
                     <div className="form-property">
                         <label htmlFor="input-property-email">Email di contatto</label>
@@ -235,13 +331,14 @@ const AddPropertyPage = () => {
                             onChange={handleInputTextChange}
                             name='email'
                         />
+                        {validationData.email && <small className="text-danger">L'email non è valida</small>}
                     </div>
                 </>
             )}
 
             {step === 5 && (
                 <div className="text-center my-3">
-                <strong>Sei sicuro di voler aggiungere questo immobile?</strong>
+                    <strong>Sei sicuro di voler aggiungere questo immobile?</strong>
                 </div>
             )}
 
