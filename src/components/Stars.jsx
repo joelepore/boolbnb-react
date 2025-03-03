8a82f15e04e7127e6f6e61580501b7c57682e6bd
Francesca-Lisi <francescalisi2143@gmail.com>
2025-03-03 16:50:16 +0100

    fix: stars color

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -4,12 +4,12 @@ const Stars = ({ vote }) => {
     const value = vote * 2 * 10
     return (
         <div className='w-100'>
-            <div className="position-relative star-bottom text-warning">
-                <i className="fa-regular fa-star"></i>
-                <i className="fa-regular fa-star"></i>
-                <i className="fa-regular fa-star"></i>
-                <i className="fa-regular fa-star"></i>
-                <i className="fa-regular fa-star"></i>
+            <div className="position-relative star-bottom">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
                 <div className="position-absolute top-0 start-0 star-top" style={{ width: value + '%' }} >
                     <i className="fa-solid fa-star"></i>
                     <i className="fa-solid fa-star"></i>
